8ca2ebbdbee593afa43c00f2211e171483fff98f
Matteo Hertel <info@matteohertel.com>
2015-12-30 00:22:57 +0000

    new folder structure

---
+++ source/js/trex/Model.js
@@ -0,0 +1,38 @@
+var Model = {
+    components: [{
+        name: "body",
+        obj: "./dist/objs/body.obj",
+        map: "./dist/img/Trex_Diffuse.jpg ",
+        normal: "./dist/img/Trex_Normal.jpg"
+    }, {
+        name: "lowGum",
+        obj: "./dist/objs/low_gum.obj",
+        map: "./dist/img/BottomGumDeffuse.jpg",
+        normal: false
+    }, {
+        name: "tongue",
+        obj: "./dist/objs/tongue.obj",
+        map: "./dist/img/TongueDeffuse.jpg",
+        normal: false
+    }, {
+        name: "webbing",
+        obj: "./dist/objs/webbing.obj",
+        map: "./dist/img/MouthWebbing.jpg",
+        normal: "./dist/img/Webbingnormal2.jpg"
+    }, {
+        name: "upGum",
+        obj: "./dist/objs/up_gum.obj",
+        map: "./dist/img/TopGumDeffuse.jpg",
+        normal: false
+    }, {
+        name: "eyes",
+        obj: "./dist/objs/eyes.obj",
+        map: "./dist/img/dinosaureye_03c.jpg",
+        normal: false
+    }, {
+        name: "tusks",
+        obj: "./dist/objs/tusks.obj",
+        map: false,
+        normal: false
+    }]
+};
