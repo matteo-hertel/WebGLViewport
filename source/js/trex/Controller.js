37b2d9f38ce3715fe2e47f88f3ee7116b3e7d5c4
Matteo Hertel <info@matteohertel.com>
2016-01-01 13:18:03 +0000

    refactor Controller structure

--- source/js/trex/Controller.js
+++ source/js/trex/Controller.js
@@ -1,40 +1,48 @@
 var Controller = Object.create(Model);
 Controller.view = Object.create(View);
-Controller.init = function() {
 
+Controller.init = function() {
     this.container = document.createElement('div');
 
+    //THREE Objects
+    this.createThreeObjects();
+    // init scene
+    this.addElementToScene();
+    //init controls
+    this.createControls();
 
+    this.container.appendChild(this.renderer.domElement);
+
+    document.body.appendChild(this.container);
+    this.animate();
+};
+Controller.createThreeObjects = function() {
+    //camera
     this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
     this.camera.position.z = 600;
-
+    //scene
+    this.scene = new THREE.Scene();
+    //renderer
+    this.renderer = new THREE.WebGLRenderer({
+        alpha: true
+    });
+    this.renderer.setSize(this.view.windowX, this.view.windowY);
+};
+Controller.createControls = function() {
     //events
     this.view.bindEvents.bind(this)();
-    // scene
+    //Controls
+    this.controls = new THREE.OrbitControls(this.camera);
+    this.controls.target.set(0, 0, 0);
+    this.controls.addEventListener("change", this.render.bind(this));
 
-    this.scene = new THREE.Scene();
-
+};
+Controller.addElementToScene = function() {
     // components
     this.view.loadComponents.bind(this)();
     this.view.drawGrid.bind(this)();
     //lights
     this.view.addLights.bind(this)();
-
-    this.renderer = new THREE.WebGLRenderer({
-        alpha: true
-    });
-
-
-    this.renderer.setSize(this.view.windowX, this.view.windowY);
-    this.container.appendChild(this.renderer.domElement);
-
-
-    this.controls = new THREE.OrbitControls(this.camera);
-    this.controls.target.set(0, 0, 0);
-    this.controls.addEventListener("change", this.render.bind(this));
-
-    document.body.appendChild(this.container);
-    this.animate();
 };
 Controller.animate = function() {
     requestAnimationFrame(this.animate.bind(this));
